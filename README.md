763e56561c2e204afa58456a6ff1cccf0afe9697
ALex Jen <alexjen1999@gmail.com>
2024-04-15 23:22:14 -0500

    init

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # oliver-getting-started
+
+1. Download Node.js: https://nodejs.org/en/download
+2. Download mySQL
+3. Change mySQL login to your user and pass
+4. run "node server.js"
+5. Open up Browser 
---
+++ app/server.js
@@ -0,0 +1,65 @@
+// Express is used for easy get and post response 
+const express = require('express')
+const app = express()
+// Child_process is for running python scripts under the hood
+const { spawn } = require('child_process');
+var path = require('path'); // Help support pathing
+// MySQL handler
+// Documentation https://sidorares.github.io/node-mysql2/docs#first-query
+var mysql = require('mysql2');
+
+const port = 8081
+
+/////////////////// CONNECTING SERVER TO MYSQL //////////////////
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  port: '3306',
+  password: "root"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+////////////////////////////////////////////////////////////////
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/html/index.html'));
+})
+
+// Example GET API that inputs any command into SQL
+app.get('/myGETAPI', (req, res) => {
+  // Grabbing the GET data sent into the website
+  response = {
+    // You can reference the index.html and see that we are Querying for the Name object
+    Name:req.query.changeMeToQuery
+  }
+  console.log("You entered: " + response.Name)
+  res.send(response.Name);
+});
+
+// Displaying database in console.log
+app.get('/showDatabase', (req, res) => {
+  con.query("SHOW DATABASES", function (err, result) {
+    var sql_res = "Displaying Databases\n"
+    if (err) {
+      throw (err)
+    } else {
+      console.log('Executed SHOW DATABASES Command Succesfully');
+      var i = 0
+      result.forEach((object) => {
+        i = i + 1
+        sql_res = sql_res + 'Database ' + i + ' ' + object.Database + '\n'
+        })
+      console.log(sql_res)
+    }
+  })
+
+  res.send("Success!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
